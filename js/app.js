e37334afcd1b564b147698141c463856040ea1df
Roudan Haj Hussein <rodan.hajhussein31@gmail.com>
2019-04-18 17:24:34 +0300

    update JS code

--- js/app.js
+++ js/app.js
@@ -1,22 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
-// select the cards
-const selectCards = document.querySelectorAll(".card");
-/* define an array that contains the card because you can't pass a nodeList to shuffle().
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from */
-const cards = Array.from(selectCards);
-// select the deck of cards
-const deck = document.querySelector(".deck");
-// To append it the shuffled deck and then append it to the .deck, so the browser will do only one reflow and one repaint
-const fragment = document.createDocumentFragment();
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -32,9 +13,12 @@ function shuffle(array) {
     return array;
 }
 
+
 function shuffleCards() {
+
 	// shuffle the cards
 	const shuffledDeck = shuffle(cards);
+
 	// append the shuffled cards to fragment
 	for (const card of shuffledDeck) {
 		fragment.appendChild(card);
@@ -44,8 +28,6 @@ function shuffleCards() {
 	deck.appendChild(fragment);
 }
 
-// will be used to check whether restart button is clicked or not.
-let isNotClicked = true;
 // show the cards for 6 seconds when starting/restarting the game.
 function startGame() {
 
@@ -65,9 +47,12 @@ function startGame() {
 
 }
 
-let removedStars = [];
+
 function removeStar() {
-	const stars = document.querySelectorAll(".fa-star"); // select the stars
+
+	// select the stars
+	const stars = document.querySelectorAll(".fa-star");
+
 	if ((movesCounter % 10 === 0) && (stars.length > 1)) { /* each time movesCounter is increased 10 numbers,
 	 one star is removed, but if there is only one star left it stops. */
 			removedStars.push(stars[0]);
@@ -75,9 +60,7 @@ function removeStar() {
 		}
 }
 
-let seconds = 0;
-let setTimeoutId; // will be used when stopping the timer.
-const timeContainer = document.querySelector(".time");
+
 function timer() {
 	if (matchedCards.length !== 16) {
 		timeContainer.innerHTML = seconds;
@@ -85,6 +68,7 @@ function timer() {
 		setTimeoutId = setTimeout(timer, 1000);
 	}
 }
+
 
 function restart() {
 
@@ -130,28 +114,30 @@ function restart() {
 
 }
 
-const congratsPopup = document.querySelector(".congrats-popup");
+
 function gameEnded() {
 	if (matchedCards.length === 16) {
 
 		setTimeout(function() {
-			// update the score time
 			const scoreTime = document.querySelector(".score-time");
+			const scoreMoves = document.querySelector(".score-moves");
+			const stars = document.querySelectorAll(".fa-star");
+			const scoreStarsText = document.querySelector(".score-stars-text");
+			const scoreStars = document.querySelector(".score-stars");
+			let starsLeft = 0;
+
+			// update the score time
 			scoreTime.innerHTML = seconds - 1;
 
 			// update the moves
-			const scoreMoves = document.querySelector(".score-moves");
 			scoreMoves.innerHTML = movesCounter;
 
 			// count the left stars
-			const stars = document.querySelectorAll(".fa-star"); // select the stars
-			let starsLeft = 0;
 			for (let star of stars) {
 				starsLeft += 1;
 			}
 
 			// if only one star is left, make sure "star" is displayed and not "stars".
-			const scoreStarsText = document.querySelector(".score-stars-text");
 			if (starsLeft === 1) {
 				scoreStarsText.innerHTML = "star";
 			} else {
@@ -159,7 +145,6 @@ function gameEnded() {
 			}
 
 			// update the stars
-			const scoreStars = document.querySelector(".score-stars");
 			scoreStars.innerHTML = starsLeft;
 
 			// show the popup
@@ -170,14 +155,9 @@ function gameEnded() {
 	}
 }
 
-shuffleCards();
 
-let movesCounter = 0;
-let matchedCards = [];
-let selectedCards = [];
-
-const container = document.querySelector(".container"); // the whole container.
-const moves = document.querySelector(".moves"); // the moves span.
+// container.
+const container = document.querySelector(".container");
 
 container.addEventListener("click", function(event) {
 
@@ -187,8 +167,9 @@ container.addEventListener("click", function(event) {
 
 			// to make sure that the player won't select the same card or the card that has the "match"class.
 			if ((selectedCards[0] === event.target) || (event.target.classList.contains("match"))) {
-				event.stopPropagation(); /* learned from:
-				 https://developer.mozilla.org/en-US/docs/Web/API/Event/stopPropagation */
+
+				/* https://developer.mozilla.org/en-US/docs/Web/API/Event/stopPropagation */
+				event.stopPropagation();
 
 			} else {
 
@@ -214,6 +195,7 @@ container.addEventListener("click", function(event) {
 
 					selectedCards[0].classList.add("dont-match");
 					selectedCards[1].classList.add("dont-match");
+
 					setTimeout(function() {
 						selectedCards[0].classList.remove("match", "open", "show", "dont-match");
 						selectedCards[1].classList.remove("match", "open", "show", "dont-match");
@@ -225,21 +207,22 @@ container.addEventListener("click", function(event) {
 			// check if the user has won the game.
 			gameEnded();
 
-		} else if ((selectedCards.length === 2) || (event.target.classList.contains("open", "show"))) {
-			/* to make sure that only 2 cards can be displayed at the same time, also when starting the game and
-			showing the cards for 6 sec.
-			learned from: https://developer.mozilla.org/en-US/docs/Web/API/Event/stopPropagation */
-			event.stopPropagation();
+		} else if ((selectedCards.length === 2) || (event.target.classList.contains("open", "show")) || (event.target.classList.contains("match"))) {
 
-		} else if ((event.target.classList.contains("match"))){ // if the clicked card has "match" class
+			/* to make sure that only 2 cards can be displayed at the same time, also when starting the game and
+			showing the cards for 6 sec. Plus if the cards already has "match" class.
+			https://developer.mozilla.org/en-US/docs/Web/API/Event/stopPropagation */
 			event.stopPropagation();
 
 		} else { // when selectedCards array is empty.
+
 			event.target.classList.add("show", "open");
 			selectedCards.push(event.target);
 			movesCounter += 1;
+
 			// check if a start should be removed.
 			removeStar();
+
 			// if this is the first card that is clicked, call the timer function
 			if (seconds === 0) {
 				timer();
@@ -261,6 +244,7 @@ container.addEventListener("click", function(event) {
 
 		// to avoid running the code when pressing space.
 		if (startPopup.classList.contains("visible")) {
+
 			// show the cards for 6 seconds
 			startGame();
 			// hide popup
@@ -270,16 +254,55 @@ container.addEventListener("click", function(event) {
 
 });
 
+// select the cards
+const selectCards = document.querySelectorAll(".card");
+
+/* define an array that contains the card because you can't pass a nodeList to shuffle().
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from */
+const cards = Array.from(selectCards);
+
+// select the deck of cards
+const deck = document.querySelector(".deck");
+
+
+// To append it the shuffled deck and then append it to .deck
+const fragment = document.createDocumentFragment();
+
+
+// will be used to check whether restart button is clicked or not.
+let isNotClicked = true;
+
+
+// removed stars.
+let removedStars = [];
+
+
+// for timing.
+let seconds = 0;
+// will be used when stopping the timer.
+let setTimeoutId;
+const timeContainer = document.querySelector(".time");
+
+
+// congrats popup
+const congratsPopup = document.querySelector(".congrats-popup");
+
+
+// moves counter
+let movesCounter = 0;
+
+// matched cards
+let matchedCards = [];
+
+// clicked cards
+let selectedCards = [];
+
+// the moves span.
+const moves = document.querySelector(".moves");
+
 // show start popup
 const startPopup = document.querySelector(".start-popup");
 startPopup.classList.add("visible");
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+// call shuffleCards function
+shuffleCards();
